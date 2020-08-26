dcdafa9f33e4cd9e0859b9dd0d6cee58f153b0dd
Christian Otu <christianotu.d@gmail.com>
2020-08-27 00:19:27 +0100

    Updates

---
+++ precache-manifest.56f5388fd6aed80be4f32722eccfd60a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a49eaf6604a88a18d399ea9d1d521d",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "035ed6ffa57d8332c75a",
+    "url": "/Expense-Tracker/static/css/main.3537542a.chunk.css"
+  },
+  {
+    "revision": "30f81cd6ba53f685d614",
+    "url": "/Expense-Tracker/static/js/2.40a8ce89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Expense-Tracker/static/js/2.40a8ce89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035ed6ffa57d8332c75a",
+    "url": "/Expense-Tracker/static/js/main.588e6d8b.chunk.js"
+  },
+  {
+    "revision": "a2680d444f91ca936f96",
+    "url": "/Expense-Tracker/static/js/runtime-main.990036e0.js"
+  }
+]);
